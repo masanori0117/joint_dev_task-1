b66a06a93eaea5d758f4d286ee3b3622daf53162
masanori0117 <masanori.takahashi.0117@gmail.com>
2020-04-29 19:49:40 +0900

    php basic Q1-Q8

--- task.php
+++ task.php
@@ -5,6 +5,8 @@ print("#####q1#####".PHP_EOL);
 $names = ["田中", "佐藤", "佐々木", "高橋"];
 
   # 以下に回答を記載
+  $names[] = "斎藤";
+  print_r($names);
 
 echo PHP_EOL;
 
@@ -13,6 +15,8 @@ $array1 = ["dog", "cat", "fish"];
 $array2 = ["bird", "bat", "tiger"];
 
   # 以下に回答を記載
+  $merged_array = array_merge($array1 , $array2);
+  print_r($merged_array);
 
 echo PHP_EOL;
 
@@ -20,6 +24,15 @@ print("#####q3#####".PHP_EOL);
 $numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9];
 
   # 以下に回答を記載
+  $count = 0;
+  
+  foreach ($numbers as $value) {
+    if($value === 3) {
+      $count += 1;
+    }
+  }
+  
+  echo '数字の3は' . $count . '個あります';
 
 echo PHP_EOL;
 
@@ -27,6 +40,8 @@ print("#####q4#####".PHP_EOL);
 $sports = ["サッカー", "フットサル", null, "野球", "バスケ", null, "バレー"];
 
   # 以下に回答を記載
+  $sports = array_diff($sports, [null]);
+  print_r($sports);
 
 echo PHP_EOL;
 
@@ -35,6 +50,9 @@ $array1 = [];
 $array2 = [1, 5, 8, 10];
 
   # 以下に回答を記載
+  var_export(count($array1) === 0);
+  echo PHP_EOL;
+  var_export(count($array2) === 0);
 
 echo PHP_EOL;
 
@@ -42,6 +60,11 @@ print("#####q6#####".PHP_EOL);
 $numbers1 = [1, 2, 3, 4, 5];
 
   # 以下に回答を記載
+  foreach ($numbers1 as $value) {
+    $value *= 10;
+    $numbers2[] = $value;
+  }
+  print_r($numbers2);
 
 echo PHP_EOL;
 
@@ -49,13 +72,24 @@ print("#####q7#####".PHP_EOL);
 $array = ["1", "2", "3", "4", "5"];
 
   # 以下に回答を記載
+  //var_dump($array);
+  
+  $array = array_map('intval', $array);
+  print_r($array);
+
+  //var_dump($array);
 
 echo PHP_EOL;
 
 print("#####q8#####".PHP_EOL);
 $programming_languages = ["php","ruby","python","javascript"];
 
+
+
   # 以下に回答を記載
+  foreach ($programming_languages as $language) {
+    $upper_case_programming_languages[] = strtoupper($language);    
+  }
 
   # 以下は変更しないで下さい
 print_r($programming_languages);
